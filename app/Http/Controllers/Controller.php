1d3ec7142ce850c6cb54370b17e8ff90b5cce9ea
zabeer001 <binzabirtareq@gmail.com>
2025-04-30 16:41:54 +0600

    e

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,22 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected function responseSuccess($data, $message = 'Request successful', $code = 200)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    protected function responseError($message = 'Request failed', $error = null, $code = 500)
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'error' => $error
+        ], $code);
+    }
 }
